ddd856c6c560d1b10100f2b328a4d3c777ca4337
Fabian Cook <hello@fabiancook.dev>
2019-10-13 18:50:24 +1300

    Use tsx source for example source

--- src/contents/example.tsx
+++ src/contents/example.tsx
@@ -2,18 +2,19 @@ import { h } from "../h";
 import { BROWSER } from "../environment";
 
 export type ExampleOptions = {
-  src: string;
+  runnable: string;
   key: string;
+  source?: string;
 };
 
-export async function *Example({ src, key }: ExampleOptions) {
+export async function *Example({ runnable, source, key }: ExampleOptions) {
   yield <p>Loading example</p>;
   if (!BROWSER) {
     return;
   }
   const [imported, response] = await Promise.all([
-    import(src),
-    fetch(src)
+    import(runnable),
+    fetch(source || runnable)
   ]);
   const text = await response.text();
   const Component = imported[key];
@@ -23,7 +24,7 @@ export async function *Example({ src, key }: ExampleOptions) {
         <Component />
       </div>
       <pre>
-        {text.replace(/\/\/# sourceMapping.+/, "").trim()}
+        {text.trim()}
       </pre>
     </fragment>
   );
