6a8eca5027d5e4a93446f1358e7924049f3fb19e
Fabian Cook <hello@fabiancook.dev>
2019-10-16 18:28:09 +1300

    remove api usage

--- src/contents/examples/async-single-render.tsx
+++ src/contents/examples/async-single-render.tsx
@@ -1,7 +1,6 @@
 import { h } from "../../h";
 
 export async function AsyncSingleRender() {
-  const response = await fetch("https://rickandmortyapi.com/api/", { mode: "cors" });
-  const result = await response.json();
-  return <p>Async result: {JSON.stringify(result, undefined, "  ")}</p>;
+  const result = await new Promise(resolve => setTimeout(() => resolve("I was fetched using a promise!"), 50));
+  return <p>Async result: {result}</p>;
 }
